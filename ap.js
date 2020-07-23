4b61701824a5aac8d1d414427892e0895e35dca2
shahid chaudhary <shahid_chaudhary@outlook.com>
2020-07-23 12:14:25 -0700

    navbar

---
+++ ap.js
@@ -0,0 +1,8 @@
+const navSlide = () => {
+	const burger = document.querySelecter('.burger');
+	const burger = document.querySelecter('.nav-links');
+	burger.addEventlistner('click',() =>{
+		nav-classList.toggle('nav-active');
+	});
+}
+navSlide();
